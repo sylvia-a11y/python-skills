d59eb5f5f88a39db2484fdd781681d4816ea2e2d
sylvia <sylvianassazi56@gmail.com>
2026-03-27 22:34:28 +0300

    add slicing.py with list slicing syntax and rules

---
+++ 08_dashboard_pagination/slicing.py
@@ -0,0 +1,8 @@
+#syntaxName: List slice
+#Format: List [start:stop]
+#CRITICAL RULES!!!!
+#1. "Start" is inclusive
+#2. "Stop" is exclusive(stop before the stop index)
+#3. If you omit "start", python will start from the beginning of the list(list[:stop])
+#4. If you omit "stop", python will go until the end of the list(list[start:])
+#5. Negative indices, python counts from the wright side
